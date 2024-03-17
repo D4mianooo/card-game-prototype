74fa95d2997e528d7ef9168c0ace92f2f0a6e395
Damian Jasek <damiannq1337@gmail.com>
2024-03-17 20:59:00 +0100

    refactor(unit scripts): refactor unit scripts

--- Assets/Scripts/Tile/Tile.cs
+++ Assets/Scripts/Tile/Tile.cs
@@ -1,21 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using TMPro;
+
 using UnityEngine;
 
-public class Tile : MonoBehaviour {
-    [SerializeField] private GameObject _unit = null;
-    
-    public bool IsOccupied() {
-        return _unit != null;
-    }
-    public void AlignUnit([CanBeNull] GameObject unit) {
-        _unit = unit;
-    }
-    
-    
-    
-
+public class Tile : MonoBehaviour { 
+    public bool _isBusy = false;
 }
--- Assets/Scripts/Unit/UnitSpawnHandler.cs
+++ Assets/Scripts/Unit/UnitSpawnHandler.cs
@@ -4,25 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Utils;
 public class UnitSpawnHandler : MonoBehaviour {
-     [SerializeField] private GameObject _unitPrefab;
-     [SerializeField] private Tile _tile;
-     private void Awake() {
-         _tile = GetComponentInParent<Tile>();
-     }
-     private void OnMouseOver() {
-         if (Input.GetMouseButtonDown(0)) {
-             if (_tile.IsOccupied()) return;
-             // throwEvent;
-             SpawnUnit();
+    [SerializeField] private Unit _unit; 
+     private void Update() {
+         if(Input.GetMouseButtonDown(0)) {
+             Vector3 spawnPosition = Vectors.Round(transform.position);
+             spawnPosition.y = 0f; 
+             Tile tile = Board.instance.GetTile(spawnPosition);
              
+             if(tile == null) return;
+             if(tile._isBusy) return;
+             SpawnUnit(_unit, spawnPosition);
+             tile._isBusy = true;
          }
      }
-     private void SpawnUnit() {
-         Vector3 mouse = Mouse3D.GetMouseWorldPosition();
-         Vector3 spawn = Vectors.Round(mouse);
-         spawn.y = 0f;
-         GameObject unit = Instantiate(_unitPrefab, spawn, Quaternion.identity);
-         _tile.AlignUnit(unit);
-
+     private void SpawnUnit(Unit unit, Vector3 spawnPosition) {
+         Instantiate(unit, spawnPosition, Quaternion.identity);
      }
 }
