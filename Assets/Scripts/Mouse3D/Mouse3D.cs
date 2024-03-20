44061ca203b3eebcab9068ab463c9ff2a0964f65
Damian Jasek <damiannq1337@gmail.com>
2024-03-20 19:19:04 +0100

    Use Singleton on Board and Mouse3D class, Change Vector3 to Vector2

--- Assets/Scripts/Mouse3D/Mouse3D.cs
+++ Assets/Scripts/Mouse3D/Mouse3D.cs
@@ -2,21 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utils;
 
 public class Mouse3D : MonoBehaviour {
+
+    public static Mouse3D Instance;
+    private void Start() {
+        Instance = this;
+    }
     private void FixedUpdate() {
         transform.position = GetMouseWorldPosition();
     }
-    public static Vector3 GetMouseWorldPosition() {
+    public Tile GetTileOnMouseWorldPosition() {
+        Vector3 position = Vectors.Round(transform.position);
+        position.y = 0f;
+        return Board.Instance.GetTile(position);
+
+    }
+    public  Vector3 GetMouseWorldPosition() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!Physics.Raycast(ray, out RaycastHit hit)) return Vector3.zero;
 
         return hit.point;
     }
-    public static Vector3 GetMouseWorldPosition(LayerMask _layer) {
+    public Vector3 GetMouseWorldPosition(LayerMask _layer) {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!Physics.Raycast(ray, out RaycastHit hit, float.MaxValue,_layer)) return Vector3.zero;
-
 
         return hit.point;
     } 
