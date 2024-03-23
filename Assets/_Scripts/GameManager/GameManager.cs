8a470c3b5335b413de926e7d58113a94c71f85ba
Damian Jasek <damiannq1337@gmail.com>
2024-03-23 19:29:56 +0100

    Modify GameManager

--- Assets/_Scripts/GameManager/GameManager.cs
+++ Assets/_Scripts/GameManager/GameManager.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
-
+    public event Action<GameState> OnGameStateChanged;
+    
     private GameState _currentState;
-
+    private void Awake() {
+        Instance = this;
+    }
+    private void Start() {
+        SetState(GameState.FPlayerTurn);
+    }
     public GameState GetState() {
         return _currentState;
     }
     public void SetState(GameState state) {
+        if (OnGameStateChanged != null) {
+            OnGameStateChanged(state);
+        }
         switch (state) {
             case GameState.FPlayerTurn:
+                
+                break;
+            case GameState.SPlayerTurn:
                 
                 break;
         }
